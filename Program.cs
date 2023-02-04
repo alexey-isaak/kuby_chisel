f2738c229fd6993ff3cbc2cbdbe4f479389cf377
Alexey_I <gafar@xaker.ru>
2023-02-04 16:34:59 +0300

    Itogovy sxript

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+ 
+namespace kuby_chisel
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число N: ");
+            int chisloN = int.Parse(Console.ReadLine());
+            int i = 1;
+            while ( i < chisloN )
+            {
+                double kub = Math.Pow(i, 3);
+                Console.WriteLine("Число " + i + " куб числа " + i + "  " + kub);
+                i = i + 1;
+            }
+        }
+    }
+}
